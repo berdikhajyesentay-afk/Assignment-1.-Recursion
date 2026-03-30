a39b4479f1f2d458682dc71d27b90ccf00579819
Byerdikhajy <berdikhajyesentay@gmail.com>
2026-03-31 04:16:17 +0500

    Update

--- src/Task1.java
+++ src/Task1.java
@@ -1,12 +1,20 @@
 public class Task1 {
+
     public static int sumSquares(int n) {
         if (n <= 0) return 0;
-        if (n == 1) return 1;
         return (n * n) + sumSquares(n - 1);
+    }
+
+
+    public static String getExpression(int n) {
+        if (n <= 1) return "1^2";
+        return getExpression(n - 1) + " + " + n + "^2";
     }
 
     public static void main(String[] args) {
         int n = 4;
-        System.out.println("Result: " + sumSquares(n));
+        if (n > 0) {
+            System.out.println(getExpression(n) + " = " + sumSquares(n));
+        }
     }
 }
